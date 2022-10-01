964cae615c38cf3f78882ba5ce4f1a60a2761fc2
星空 <www.zijinfeihong@qq.com>
2022-10-01 18:48:27 +0800

    init
    first code2

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-actuator/src/main/java/smoketest/actuator/Example.java
@@ -0,0 +1,22 @@
+package smoketest.actuator;
+
+
+	import org.springframework.boot.*;
+            import org.springframework.boot.autoconfigure.*;
+            import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+        System.out.println("program exit.");
+    }
+
+}
